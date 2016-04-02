db60d4a469434c81e6f4cf8068343da3bbdacf1a
abdallahozaifa <abdallahozaifa19527@gmail.com>
2016-04-02 00:05:12 -0400

    Add validation input functions, and detection of object or string

--- pennstate-api.js
+++ pennstate-api.js
@@ -9,6 +9,8 @@
     var querystring = require('querystring');
     var cheerio = require('cheerio');
     
+    // var $ = require('jquery');
+    
   /**
    * psd-api namespace
    *
@@ -38,9 +40,34 @@
         
         var dirLink = 'http://www.work.psu.edu/cgi-bin/ldap/ldap_query.cgi';
         
+        priv.validateOptions = function(str){
+            return /^[a-zA-Z]+$/.test(str);
+        }
+        priv.studentInfo = {}
+        priv.optValid = true;
+        
+        /* Gets the students information passed in as options */
+        priv.getStudentInfo = function(){
+            if(typeof options === "object"){
+                for(var optKey in options){
+                    if((typeof options[optKey] != "string") || (priv.validateOptions(options[optKey]) === false)){
+                        console.error("Invalid Object format!");
+                        priv.optValid = false;
+                    }
+                }
+                priv.optValid === true ? Object.assign(priv.studentInfo,options) : null;
+            }else if(typeof options === "string"){
+                var input = options.split(" ");
+                if(input.length === 2){
+                    priv.studentInfo.firstName = input[0];
+                    priv.studentInfo.lastName = input[1];
+                }
+                else priv.studentInfo.firstName = input[0];
+            }
+        }();  
         /* form object that contains the form data that will be sent in the post request */
         var form = {
-            data: priv.newQuery("Hozaifa", "Abdalla","","")
+            data: priv.newQuery(priv.studentInfo.firstName, priv.studentInfo.lastName,"","")
         };       
         var stringFormData = querystring.stringify(form.data);
         var contentLength = stringFormData.length;
@@ -75,7 +102,7 @@
             var $ = cheerio.load(res.body);            
             /* Queries the student page for the body */
             $("body").filter(function(){
-                console.log($(this).text());
+                // console.log($(this).text());
             });
             
         };
@@ -84,7 +111,7 @@
         request(priv.reqOptions, callback);
     };
     
-    psd();
+    psd("Hozaifa");
     
     app.listen(8080, function () {
         console.log('App listening on port 8080!');
