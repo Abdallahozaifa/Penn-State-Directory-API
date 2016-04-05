0919a09b6e5777715a2aad0819d955df72c0f731
Hozaifa Abdalla <abdallahozaifa19527@gmail.com>
2016-04-05 17:06:33 +0000

    Fix funtion calls in test file

--- Test.js
+++ Test.js
@@ -4,18 +4,15 @@ var psd = require("./psd-api.js");
 
 /* SAMPLE OBJECT TESTCASES */
 /* PASSED */
-// psd({lastName: "Abdalla"}); /* PASSED */
-// psd({email: "hea113"}); /* PASSED */
-// psd({userID: "hea113"}); /* PASSED */
-// psd({firstName: "Hozaifa", lastName: ""}); /* PASSED */
-// psd({firstName: "", lastName: "Abdalla"}); /* PASSED */
-// psd({firstName: "Hozaifa", lastName: "Abdalla"}); /* PASSED */
-// psd({firstName: "Hozaifa", lastName: "Abdalla" , userID: "hea113"}); /* PASSED */
-// psd({firstName: "Hozaifa", lastName: "Abdalla" , userID: ""}); /* PASSED */
-// psd({firstName: "Hozaifa", lastName: "Abdalla" , userID: "hea113", email: "hea113@psu.edu"}); /* PASSED */
-// psd({firstName: "Hozaifa", lastName: "Abdalla" , userID: "", email: "hea113@psu.edu"}); /* PASSED */
-// psd({
-//     email: "yehya@psu.edu"
-// });
-
+// psd.get({lastName: "Abdalla"}); /* PASSED */
+// psd.get({email: "hea113"}); /* PASSED */
+// psd.get({userID: "hea113"}); /* PASSED */
+// psd.get({firstName: "Hozaifa", lastName: ""}); /* PASSED */
+// psd.get({firstName: "", lastName: "Abdalla"}); /* PASSED */
+// psd.get({firstName: "Hozaifa", lastName: "Abdalla"}); /* PASSED */
+// psd.get({firstName: "Hozaifa", lastName: "Abdalla" , userID: "hea113"}); /* PASSED */
+// psd.get({firstName: "Hozaifa", lastName: "Abdalla" , userID: ""}); /* PASSED */
+// psd.get({firstName: "Hozaifa", lastName: "Abdalla" , userID: "hea113", email: "hea113@psu.edu"}); /* PASSED */
+// psd.get({firstName: "Hozaifa", lastName: "Abdalla" , userID: "", email: "hea113@psu.edu"}); /* PASSED */
+// psd.get({email: "yehya@psu.edu"});
 psd.get("Hozaifa");
